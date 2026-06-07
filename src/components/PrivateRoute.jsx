712b4bb7d8aa2ff615b70418faa3de6fac868ed4
jannat337 <jannatulferdauz3@gmail.com>
2026-06-07 11:45:53 +0600

    update PrivateRoute to use Spinner component

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,16 +1,13 @@
 import { Navigate, useLocation } from 'react-router-dom'
 import useAuth from '../hooks/useAuth'
+import Spinner from './Spinner'
 
 const PrivateRoute = ({ children }) => {
   const { user, loading } = useAuth()
   const location = useLocation()
 
   if (loading) {
-    return (
-      <div className="min-h-screen flex items-center justify-center">
-        <div className="w-16 h-16 border-4 border-green-500 border-t-transparent rounded-full animate-spin"></div>
-      </div>
-    )
+    return <Spinner />
   }
 
   if (!user) {
