0770f5a278f1d8a941efb743170b1fadc3a65f03
jannat337 <jannatulferdauz3@gmail.com>
2026-06-07 11:40:05 +0600

    add reusable Spinner component

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,9 @@
+const Spinner = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="w-16 h-16 border-4 border-green-500 border-t-transparent rounded-full animate-spin"></div>
+    </div>
+  )
+}
+
+export default Spinner
